7de353bc4635ccf8732ad60643afb403a5f5900c
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-11-29 23:36:03 +0700

    burger menu has been created

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import './sass/_app.scss';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".header-burger");
+    const header = document.querySelector("header");
+    const jalousie = document.querySelector(".jalousie");
+    const html = document.documentElement;
+
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        html.classList.toggle("lock");
+        header.classList.toggle("active");
+        jalousie.classList.toggle("active");
+    });
+
+    jalousie.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        html.classList.toggle("lock");
+        header.classList.toggle("active");
+        jalousie.classList.toggle("active");
+    });
+});
