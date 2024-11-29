0934a3d2540691699b4cfae1d136f98ebad6d340
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-11-29 23:44:30 +0700

    a little bit fix

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import './sass/_app.scss';
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector(".header-burger");
     const header = document.querySelector("header");
+    const menu = document.querySelectorAll(".header__bottom li");
     const jalousie = document.querySelector(".jalousie");
     const html = document.documentElement;
 
@@ -19,4 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
         header.classList.toggle("active");
         jalousie.classList.toggle("active");
     });
+
+    menu.forEach((item) => {
+        item.addEventListener("click", () => {
+            burger.classList.toggle("active");
+            html.classList.toggle("lock");
+            header.classList.toggle("active");
+            jalousie.classList.toggle("active");
+        });
+    })
 });
